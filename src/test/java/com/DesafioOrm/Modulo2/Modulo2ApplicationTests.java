73398bfa95f9c8a913cfd011d7ade73240adaad1
Luann2003 <lvictorluciano@outlook.com>
2023-07-26 18:54:12 -0300

    first

---
+++ src/test/java/com/DesafioOrm/Modulo2/Modulo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DesafioOrm.Modulo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
